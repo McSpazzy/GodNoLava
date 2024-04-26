76f385e05996b1b324c1e60a7ca19bb996dbda8b
Andy <andy.g@bepoz.com.au>
2024-04-26 11:39:29 +1000

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace GodNoLava
+{
+    [BepInPlugin("org.ssmvc.valheim.godnolava", "God No Lava", "1.0.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class GodNoLava : BaseUnityPlugin
+    {
+        private static Harmony harmony;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin org.ssmvc.valheim.godnolava is loaded!");
+            harmony = new Harmony("org.ssmvc.valheim.godnolava");
+            harmony.PatchAll();
+        }
+
+        private void OnDestroy()
+        {
+            GodNoLava.harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(Player), "SetGodMode")]
+        private static class GodNoLavaMode
+        {
+            public static void Prefix(Player __instance, bool godMode)
+            {
+                __instance.m_tolerateFire = godMode;
+
+            }
+        }
+    }
+}
